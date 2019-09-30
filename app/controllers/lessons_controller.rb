607ecae6df1197a6774adf98720e604d0eb4ad17
Christian T <weareme99@gmail.com>
2019-09-30 20:48:26 +0000

    started to add strip support

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course
+
   def show
   end
 
   private
+
+  def require_enrollment_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You have not yet enrolled in this Course'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
